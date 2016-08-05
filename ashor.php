3bceebd1e97ff1bb367f1d872436d70b53644895
NuclearFej <j@fej.io>
2016-08-05 01:18:32 -0400

    Initial public release

---
+++ ashor.php
@@ -0,0 +1,118 @@
+<?php
+/*
+    Ashor, a dead-simple blogging platform, version 0.4.
+    Copyright © 2016 Jeff Meli. See https://fej.io for contact information.
+
+    Ashor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Ashor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Ashor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+echo "Ashor by Jeff Meli, licensed under AGPLv3. Visit fej.io for info.\n";
+
+require "parser.php";
+
+$files = scandir("posts");
+if ($files === FALSE)
+    die("Couldn't scan the posts folder.");
+
+$indexNum = 0;
+
+//Loop through every post.ashor file and spit out an HTML file.
+foreach ($files as $file) {
+    if (!strcasecmp(substr($file, strlen($file) - 6), ".ashor")) {
+        $source = fopen("posts/$file", "r") or die("I couldn't open source file $file.");
+        $text = fread($source, filesize("posts/$file"));
+        fclose($source);
+
+        $postText = $title = $dateStr = $dateFromSource = $postID = "";
+
+        //Copy everything in the first line to the title string, excluding the \n at the end.
+        for ($i = 0; $text[$i] != "\n"; $i++)
+            if (ord($text[$i]) >= ord(" "))
+                $title .= $text[$i];
+        $i++; //Increment to get to the next line (in other words, skip the \n)
+
+        //Do the same as above for the date *string*.
+        for (; $text[$i] != "\n"; $i++)
+            if (ord($text[$i]) >= ord(" "))
+                $dateFromSource .= $text[$i];
+        $i++;
+
+        //Having the date as an object is useful for parsing it, both for the byline and for the <time> tag.
+        $date = date_create($dateFromSource);
+        $dateStr = "posted on the " . $date->format("jS") . " of " . $date->format("F") . ", " . $date->format("Y");
+        if (!$date)
+            die("I couldn't parse the date. Try fixing it.");
+
+        //And finally for the post ID.
+        for (; $text[$i] != "\n"; $i++)
+            if (ord($text[$i]) >= ord(" "))
+                $postID .= $text[$i];
+        $i++;
+
+        $postText .= "<div class='ashor-title'>" . parse($title, $postID, TRUE)["html"] . "</div>\n";
+        $postText .= "<div class='ashor-date'>" . parse($dateStr, $postID, TRUE)["html"] . "</div>\n";
+
+        //For grabbing content to be placed before the fold, using substr().
+        $firstCharOfBody = $i;
+
+        $parsed = parse($text, $postID, FALSE, $i);
+        $postText .= $parsed["html"];
+        $beforeTheFoldText = substr($text, $firstCharOfBody, $parsed["beforeTheFoldPos"] - $firstCharOfBody);
+        $index[$indexNum++] = ["title" => $title, "dateStr" => $dateStr,
+            "dateObj" => $date, "beforeTheFoldText" => $beforeTheFoldText, "postID" => $postID];
+
+        $postTemplateFile = fopen("templates/post-template.html", "r") or die("I couldn't read the post template.");
+        $postTemplate = fread($postTemplateFile, filesize("templates/post-template.html"));
+        fclose($postTemplateFile);
+        $finalPost = str_replace('<!-- Ashor post -->', $postText, $postTemplate, $numOfReplacements);
+        if (!$numOfReplacements)
+            echo "I couldn't find the special token in the post template.";
+
+        $html = fopen("blog/$postID.html", "w") or die("I couldn't write an HTML file.");
+        fwrite($html, $finalPost);
+        fclose($html);
+    }
+}
+
+//The strcmp comparison is intentionally reversed to order posts by newest-to-oldest.
+if (!usort($index, function ($post1, $post2) { return strcmp($post2["postID"], $post1["postID"]); }))
+    die("Couldn't sort the posts, for some reason.");
+
+$indexHTML = "<main class='ashor-index'>\n";
+foreach ($index as $post) {
+    $indexHTML .=
+        "    <section class='ashor-index-post'>\n" .
+        "        <div class='ashor-index-title'>{$post["title"]}</div>\n" .
+        "        <time class='ashor-index-date' datetime='" . $post["dateObj"]->format("Y-m-d") . "'>" .
+        "{$post["dateStr"]}</time>\n" .
+        "        <div class='ashor-index-content'>{$post["beforeTheFoldText"]}</div>\n" .
+        "        <a class='ashor-index-link'>Continue reading</a>\n";
+        "    </section>\n";
+}
+$indexHTML .= "</main>\n";
+
+$indexTemplatePath = "templates/index-template.html";
+$indexTemplateFile = fopen($indexTemplatePath, "r") or die("I couldn't read the post template.");
+$indexTemplate = fread($indexTemplateFile, filesize($indexTemplatePath));
+$indexReplaced = str_replace('<!-- Ashor index -->', $indexHTML, $indexTemplate, $numOfReplacements);
+if ($numOfReplacements > 1)
+    echo "[Warning] More than one index marker was found. Check your index template.";
+
+$indexFile = fopen("blog-index.html", "w") or die("I couldn't write an HTML file. (2)");
+fwrite($indexFile, $indexReplaced);
+fclose($indexFile);
+
+echo "...done.\n";
+
+
---
+++ parser.php
@@ -0,0 +1,114 @@
+<?php
+/*
+    Ashor, a dead-simple blogging platform.
+    Copyright © 2016 Jeff Meli. See https://fej.io for contact information.
+
+    Ashor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    Ashor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with Ashor.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class Tag extends SplEnum {
+    const Bold = 1;
+    const Italics = 2;
+    const Header = 3;
+    const BeforeTheFold = 4;
+    const Picture = 5;
+    const Link = 6;
+}
+
+/*  Parses Ashor text and returns an array with these elements:
+    "html" => String of parsed HTML text
+    "beforeTheFoldPos" => Position of the backslash in the final "\f" found,
+                          or -1 if no "\f" was found.
+    Starts parsing the string $toParse from index $pos. */
+function parse($text, $postID, $oneLine = FALSE ,$pos = 0) {
+    $newline = 0;
+    $escaped = false;
+    $tagStack[] = array();
+
+    if (!$oneLine)
+        $ret["html"] = "<p class='ashor-p'>";
+    else
+        $ret["html"] = "";
+    $ret["beforeTheFoldPos"] = -1;
+
+    for (; $pos < strlen($text); $pos++) {
+        if ($text[$pos] == "\\")
+            $escaped = true;
+        elseif ($text[$pos] == "\n")
+            $newline++;
+        elseif (ord($text[$pos]) >= 32) { //32 or 0x20 is the space character.
+            if ($newline == 1) {
+                $ret["html"] .= "\n";
+                $newline = 0;
+            } elseif ($newline >= 2){
+                $ret["html"] .= "</p>\n<p class=\"ashor-p\">";
+                $newline = 0;
+            }
+
+            if ($escaped) {
+                if ($text[$pos] == "]") {
+                    switch (array_pop($tagStack)) {
+                        case Tag::Bold:
+                        case Tag::Italics:
+                        case Tag::Header:
+                            $ret["html"] .= "</span>";
+                            break;
+                        case Tag::Link:
+                            $ret["html"] .= "</a>";
+                            break;
+                        default:
+                            echo "[Warning] Closed a tag where there was none.";
+                    }
+                } elseif ($text[$pos] == "b" || $text[$pos] == "B") {
+                    $ret["html"] .= "<span class=\"ashor-bold\">";
+                    $tagStack[] = Tag::Bold;
+                } elseif ($text[$pos] == "i" || $text[$pos] == "I") {
+                    $ret["html"] .= "<span class=\"ashor-italics\">";
+                    $tagStack[] = Tag::Italics;
+                } elseif ($text[$pos] == "h" || $text[$pos] == "H") {
+                    $ret["html"] .= "<span class=\"ashor-header\">";
+                    $tagStack[] = Tag::Header;
+                } elseif ($text[$pos] == "p" || $text[$pos] == "P") {
+                    $innards = parseTagWithAttribute($text, $pos);
+                    $ret["html"] .= "<img class='ashor-img' src='blog/img/$postID/{$innards["attrib"]}'>";
+                    $pos = $innards["pos"]; //$pos is the ending ']', which will then be immediately incremented.
+                } elseif ($text[$pos] == "l" || $text[$pos] == "L") {
+                    $innards = parseTagWithAttribute($text, $pos);
+                    $ret["html"] .= "<a class='ashor-link' href='{$innards["attrib"]}'>";
+                    $pos = $innards["pos"];
+                    $tagStack[] = Tag::Link;
+                } elseif ($text[$pos] == "f" || $text[$pos] == "F")
+                    $ret["beforeTheFoldPos"] = $pos - 1; //$pos - 1 accounts for the extra 'h' character.
+                elseif ($text[$pos] == "\\")
+                    $ret["html"] .= "\\";
+                else
+                    echo "This character was unduly escaped: " . $text[$pos];
+                $escaped = false;
+            } else
+                $ret["html"] .= $text[$pos];
+        }
+    }
+    if (!$oneLine)
+        $ret["html"] .= "</p>";
+    return $ret;
+}
+
+// Returns the string between both brackets, as well as the index of the ending bracket.
+function parseTagWithAttribute($str, $startPos) {
+    if ($str[$startPos] != "[")
+        die("There's a misformatted tag at location $startPos.");
+    $ret["pos"] = strpos($str, "]", ++$startPos);
+    $ret["attrib"] = substr($str, $startPos, $startPos - $ret["pos"]);
+    return $ret;
+}
